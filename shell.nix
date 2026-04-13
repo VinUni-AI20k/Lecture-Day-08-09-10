f888ae75e43025dc49985d9fbc737d8d2b09f69c
stardust-zip <nguyenhieu0802@proton.me>
2026-04-13 16:19:25 +0700

    feat: update env

--- shell.nix
+++ shell.nix
@@ -2,16 +2,15 @@ with import <nixpkgs> { };
 pkgs.mkShell {
   name = "lab8";
 
-  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-    stdenv.cc.cc
+  # Use LD_LIBRARY_PATH so pre-compiled pip/uv wheels can find the C-libraries
+  LD_LIBRARY_PATH = lib.makeLibraryPath [
+    stdenv.cc.cc.lib
     zlib
   ];
 
-  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
   packages = with pkgs; [
     uv
   ];
-
 }
